e7a82b474a80fafd131fdcdf09a214e2f8a16d34
韩文博 <9476400@qq.com>
2018-10-25 23:33:48 +0800

    no message

---
+++ index.js
@@ -0,0 +1,14 @@
+import fetchData from './fetch'
+import fetchStatus from './fetchStatus'
+import storageModule from './storage'
+import stateHoc from './stateHoc'
+// import * as publiction from './public-function'
+export {initLibraryConfig} from './config'
+
+export {
+    fetchData,
+    fetchStatus,
+    storageModule,
+    stateHoc,
+    // publiction
+}
