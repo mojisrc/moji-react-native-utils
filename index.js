e08a37ec5a38de6c7d65076a25716a2959701631
韩文博 <9476400@qq.com>
2018-11-26 23:47:49 +0800

    导出config

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import fetchData from './fetch'
 import fetchStatus from './fetchStatus'
 import storageModule from './storage'
 import stateHoc from './stateHoc'
-// import * as publiction from './public-function'
-export {initLibraryConfig} from './config'
+import { initLibraryConfig, config } from './config'
 
 export {
     fetchData,
     fetchStatus,
     storageModule,
     stateHoc,
-    // publiction
+    initLibraryConfig,
+    config
 }
